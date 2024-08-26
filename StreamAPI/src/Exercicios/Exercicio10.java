34e56fb1f68c23ffe4c15a32a9052407370b337a
Kaique Parente <kaique.parente.game@outlook.com>
2024-08-26 00:16:15 -0300

    Exercicio 10 - Resolvido

---
+++ StreamAPI/src/Exercicios/Exercicio10.java
@@ -0,0 +1,14 @@
+package Exercicios;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Exercicio10 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+        
+        List<Integer> divisiveis = numeros.stream().filter(n -> n%3==0 || n%5==0).sorted().toList();
+        divisiveis.forEach(n -> System.out.print(n + " "));
+        System.out.println("");
+    }
+}

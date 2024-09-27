caebddfa9e409742f6a704ad262879c06cf0097e
Kaique Parente <kaique.parente.game@outlook.com>
2024-09-27 01:02:32 -0300

    Ordenação

---
+++ JUnit/Pratica01/src/test/java/com/kaiqueps/pratica01/EscolhendoOrdemTest.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.kaiqueps.pratica01;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+/**
+ *
+ * @author kaiqu
+ */
+@TestMethodOrder(MethodOrderer.DisplayName.class)// - nome do display
+//@TestMethodOrder(MethodOrderer.MethodName.class) - nome do método
+//@TestMethodOrder(MethodOrderer.OrderAnnotation.class) - Order(1)
+//@TestMethodOrder(MethodOrderer.Random.class) - random
+public class EscolhendoOrdemTest {
+    
+    @DisplayName("Teste que valida se o usuário foi criado")
+    @Test
+    void validaFluxoA() {
+        Assertions.assertTrue(true);
+    }
+
+    @DisplayName("B")
+    @Test
+    void validaFluxoB() {
+        Assertions.assertTrue(true);
+    }
+
+    @DisplayName("C")
+    @Test
+    void validaFluxoC() {
+        Assertions.assertTrue(true);
+    }
+
+    @DisplayName("D")
+    @Test
+    void validaFluxoD() {
+        Assertions.assertTrue(true);
+    }
+    
+}
